0ab579daf2346a7ae7f0405d2ea9b4cd150a063a
LewisMatos <lewm20@gmail.com>
2015-10-21 18:03:39 -0400

    devise views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,6 +22,7 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+	require "omniauth-facebook"
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
@@ -236,7 +237,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-	config.omniauth :facebook, "893566727379139", "4e07450ff1778312749fb1f64d687d92"	
+	config.omniauth :facebook, "893566727379139", "4e07450ff1778312749fb1f64d687d92"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
