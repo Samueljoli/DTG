04b4e5b107009e9f81524ea1682405dfef734dfd
LewisMatos <lewm20@gmail.com>
2015-10-22 09:18:00 -0400

    implemented facebook, added new attributes to user, email,name,photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ActiveRecord::Base
 				 :omniauthable, :omniauth_providers => [:facebook]
 
 	def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+		where(provider: auth.provider, uid: auth.uid.to_s).first_or_create do |user|
         user.provider = auth.provider
-				binding.pry
         user.uid = auth.uid
+				user.name = auth.info.name
         user.email = auth.info.email
+				user.image = auth.info.image
         user.password = Devise.friendly_token[0,20]
       end
   end
